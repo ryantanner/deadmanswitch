8d172cff74f09937e8412a4347e7c7a39c5355ef
ryantanner <ryan.tanner@gmail.com>
2013-11-23 00:26:11 -0600

    Initial commit on dead mans switch

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -5,6 +5,9 @@ import spray.routing._
 import spray.http._
 import MediaTypes._
 
+import java.security.MessageDigest
+import java.math.BigInteger
+
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
 class MyServiceActor extends Actor with MyService {
@@ -24,17 +27,48 @@ class MyServiceActor extends Actor with MyService {
 trait MyService extends HttpService {
 
   val myRoute =
-    path("") {
+    pathPrefix("heartbeat" / Segment / Segment / LongNumber) { (endpointId: String, secretValue: String, timestamp: Long) =>
       get {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
+        if (authenticate(endpointId, secretValue, timestamp)) {
+          respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+            complete {
+              <html>
+                <body>
+                  Successful heartbeat
+                </body>
+              </html>
+            }
+          }
+        } else {
+          respondWithStatus(401) {
+            complete {
+              <html>
+                <body>
+                  Could not authenticate heartbeat
+                </body>
+              </html>
+            }
           }
         }
       }
     }
+
+  def authenticate(endpointId: String, secretValue: String, timestamp: Long): Boolean = {
+    val expectedTime = System.currentTimeMillis
+    val sharedSecret = getSharedSecret(endpointId)
+    val hash = {
+      val digest = MessageDigest.getInstance("MD5").digest((sharedSecret + timestamp.toString).getBytes)
+      val bigInt = new BigInteger(1, digest)
+      println(bigInt.toString(16))
+      bigInt.toString(16)
+    }
+
+    (timestamp > (expectedTime - 5000) && (timestamp < (expectedTime + 5000)) &&
+      hash == secretValue)
+  }
+
+  def getSharedSecret(endpointId: String): String = {
+    "secret"
+  }
+
 }
--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -5,28 +5,41 @@ import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
+import java.security.MessageDigest
+import java.math.BigInteger
+
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
   
   "MyService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
-        responseAs[String] must contain("Say hello")
+    val time = System.currentTimeMillis.toString
+    val secretValue = {
+      val digest = MessageDigest.getInstance("MD5").digest(("secret" + time).getBytes)
+      val bigInt = new BigInteger(1, digest)
+      println(bigInt.toString(16))
+      bigInt.toString(16)
+      //bigInt.toString(16).reverse.padTo(32, "0").reverse.mkString
+    }
+
+    "return success for valid heartbeats" in {
+      Get(s"/heartbeat/myEndpointId/$secretValue/$time") ~> sealRoute(myRoute) ~> check {
+        responseAs[String] must contain("Successful heartbeat")
       }
     }
 
-    "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
-        handled must beFalse
+    "return 401 for invalid timestamps" in {
+      val badTime = (System.currentTimeMillis - 10000).toString
+      Get(s"/heartbeat/myEndpointId/$secretValue/$badTime") ~> sealRoute(myRoute) ~> check {
+        status === Unauthorized
       }
     }
 
-    "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
-        status === MethodNotAllowed
-        responseAs[String] === "HTTP method not allowed, supported methods: GET"
+    "return 401 for invalid secrets" in {
+      Get(s"/heartbeat/myEndpointId/fdafdsafadfasdf/$time") ~> sealRoute(myRoute) ~> check {
+        status === Unauthorized
       }
     }
+
   }
 }
